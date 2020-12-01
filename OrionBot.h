798fc5e23e9750386eb13a352cf2ef2e521a0ad1
jha8 <42952573+jha8@users.noreply.github.com>
2020-12-01 10:40:25 -0700

    Add joe's strategy

--- OrionBot.h
+++ OrionBot.h
@@ -28,7 +28,7 @@ private:
 	bool TryBuildSupplyDepot();
 	bool TryBuildBarracks();
 	//ADDED
-	bool OrionBot::TryBuildStructureTest(ABILITY_ID ability_type_for_structure, UNIT_TYPEID unit_type)
+	bool OrionBot::TryBuildStructureTest(ABILITY_ID ability_type_for_structure, UNIT_TYPEID unit_type);
 
 	//Global State Tracker
 	//Lets us know what strategy we are playing
@@ -44,10 +44,12 @@ private:
 	struct finalStrategy {
 		finalStrategy() :
 			upgradeOrbital(false), newCommandCentre(false), currentBuild(0),
-			expand(false), attacking(false), num_units_scouting(0) {}
+			expand(false), attacking(false), num_units_scouting(0), morph_techlab(false), produce_banshee(false) {}
 		bool upgradeOrbital;
 		bool newCommandCentre;
 		int currentBuild;
+		bool morph_techlab;
+		bool produce_banshee;
 		bool expand;
 		int num_units_scouting;
 		bool attacking;
@@ -75,7 +77,7 @@ private:
 	struct Rax6 {
 		Rax6() :
 			upgradeOrbital(false), newCommandCentre(false), currentBuild(0),
-			expand(false), attacking(false), num_units_scouting(0)  {}
+			expand(false), attacking(false), num_units_scouting(0) {}
 		bool upgradeOrbital;
 		bool newCommandCentre;
 		int currentBuild;
